66c83c9399ac299ec9ae217aa63b2bb7badebfd7
Rcastagnola <r.castagnola00@gmail.com>
2018-10-13 15:20:13 -0300

    .

--- TP2/tp2parte3B.py
+++ TP2/tp2parte3B.py
@@ -3,14 +3,6 @@ import matplotlib.pylab as plt
 import scipy.signal as signal
 import statistics as stats
 
-def Rectangular (N,x):
-    
-    ventana = signal.boxcar(N)
-    
-    salida = np.multiply(x,ventana)
-
-    return salida
-
 def Bartlett (N,x):
     
     ventana = signal.bartlett(N)
@@ -42,6 +34,7 @@ def Flattop (N,x):
     salida = np.multiply(x,ventana)
 
     return salida
+
 
 N = 1000
 fs = 1000
@@ -81,46 +74,94 @@ abs2 = np.absolute(sp2)
 abs3 = np.absolute(sp3)
 abs4 = np.absolute(sp4)
 
-Est0 = abs0.T[250,]
-Est1 = abs1.T[250,]
-Est2 = abs2.T[250,]
-Est3 = abs3.T[250,]
-Est4 = abs4.T[250,]
+interA = O0 - 4*np.pi /N
+interB = O0 + 4*np.pi /N
 
-Esp0  = stats.median(Est0)
-Esp1  = stats.median(Est1)
-Esp2  = stats.median(Est2)
-Esp3  = stats.median(Est3)
-Esp4  = stats.median(Est4)
+Est0 = abs0.T[248:253,]
+Est1 = abs1.T[248:253,]
+Est2 = abs2.T[248:253,]
+Est3 = abs3.T[248:253,]
+Est4 = abs4.T[248:253,]
+
+Est0CUA = Est0**2
+Est1CUA = Est1**2
+Est2CUA = Est2**2
+Est3CUA = Est3**2
+Est4CUA = Est4**2
+
+NewEst0 = (sum(Est0CUA)/5)**(1/2)
+NewEst1 = (sum(Est1CUA)/5)**(1/2)
+NewEst2 = (sum(Est2CUA)/5)**(1/2)
+NewEst3 = (sum(Est3CUA)/5)**(1/2)
+NewEst4 = (sum(Est4CUA)/5)**(1/2)
+
+Esp0  = stats.median(NewEst0)
+Esp1  = stats.median(NewEst1)
+Esp2  = stats.median(NewEst2)
+Esp3  = stats.median(NewEst3)
+Esp4  = stats.median(NewEst4)
 
 Sesgo0 = Esp0 - a0
-Var0  = stats.variance(Est0)
+Var0  = stats.variance(NewEst0)
 
 Sesgo1 = Esp1 - a0
-Var1  = stats.variance(Est1)
+Var1  = stats.variance(NewEst1)
 
 Sesgo2 = Esp2 - a0
-Var2  = stats.variance(Est2)
+Var2  = stats.variance(NewEst2)
 
 Sesgo3 = Esp3 - a0
-Var3  = stats.variance(Est3)
+Var3  = stats.variance(NewEst3)
 
 Sesgo4 = Esp4 - a0
-Var4  = stats.variance(Est4)
+Var4  = stats.variance(NewEst4)
 
 plt.title('Histograma Rectangular' )
-plt.hist(Est0)
+plt.hist(NewEst0)
 plt.show()
 plt.title('Histograma Bartlett' )
-plt.hist(Est1)
+plt.hist(NewEst1)
 plt.show()
 plt.title('Histograma Hann' )
-plt.hist(Est2)
+plt.hist(NewEst2)
 plt.show()
 plt.title('Histograma Blackman' )
-plt.hist(Est3)
+plt.hist(NewEst3)
 plt.show()
 plt.title('Histograma Flat-top' )
-plt.hist(Est4)
+plt.hist(NewEst4)
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- TP2/tp2ventanas.py
+++ TP2/tp2ventanas.py
@@ -2,6 +2,14 @@ import numpy as np
 import matplotlib.pylab as plt
 import scipy.signal as signal
 
+def Rectangular (N,x):
+    
+    ventana = signal.boxcar(N)
+    
+    salida = np.multiply(x,ventana)
+
+    return salida
+
 def Bartlett (N,x):
     
     ventana = signal.bartlett(N)
@@ -41,17 +49,30 @@ f0 = 10
 
 señal = 1
 
+rectangular = Rectangular(N,señal)
 bartlett = Bartlett(N,señal)
 hann     = Hann(N,señal)
 blackman = Blackman(N,señal)
 flattop  = Flattop(N,señal)
 
+sp0 = np.fft.fft(rectangular,2048)/25.5
 sp1 = np.fft.fft(bartlett,2048)/25.5
 sp2 = np.fft.fft(hann,2048)/25.5
 sp3 = np.fft.fft(blackman,2048)/25.5
 sp4 = np.fft.fft(flattop,2048)/25.5
 
 freq = np.linspace(0, 1, 100)
+
+plt.title('Ventana Rectangular' )
+plt.ylabel("Amplitude")
+plt.xlabel("Sample")
+plt.plot(rectangular)
+plt.show()
+plt.title('Espectro' )
+plt.ylabel("Magnitud [dB]")
+plt.xlabel("Frequencia")
+plt.plot(freq, 20*np.log10(np.absolute(sp0)[0:100]))
+plt.show()
 
 plt.title('Ventana Bartlett' )
 plt.ylabel("Amplitude")
